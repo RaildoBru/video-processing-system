6d68d5ca2b099786ec1bf1471d41a1ee8bf27a18
RaildoBru <raildobruno@gmail.com>
2025-04-21 20:45:03 -0300

    adicionando banco de dados mongodb

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -1,5 +1,16 @@
-const { app } = require('./app');
+//const { app } = require('./app');
+const express = require('express');
+const connectDB = require('./config/db');
+const router = require('./app');
 
-app.listen(3001, () => {
-  console.log('Auth na 3001');
-});
+const app = express();
+
+app.use(express.json());
+// Conectar ao banco
+connectDB();
+
+// Usar rotas de auth
+app.use(router);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`🚀 Auth service rodando na porta ${PORT}`));
